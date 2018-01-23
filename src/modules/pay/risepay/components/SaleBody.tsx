e2d1466121710961708973337a659529e00dde7a
richardyangcz <richardyangcz@sina.com>
2018-01-23 18:37:01 +0600

    modify rise pay pic

--- src/modules/pay/risepay/components/SaleBody.tsx
+++ src/modules/pay/risepay/components/SaleBody.tsx
@@ -155,7 +155,7 @@ export class SaleBody extends React.Component {
            id="business-school-intro-pic-container">
         <img
           className="pic-part1"
-          src="https://static.iqycamp.com/images/pay_rise_part1_11.png?imageslim"
+          src="https://static.iqycamp.com/images/pay_rise_part1_12.png?imageslim"
           style={{ width: '100%' }}
           onLoad={() => this.setState({ loading: false })}/>
         {
