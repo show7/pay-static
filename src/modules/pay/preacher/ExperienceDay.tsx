5ebd9f6de85281d156c102f06f0bf6d9ee6ac6c8
nethunder <17089113140@163.com>
2018-04-03 20:46:43 +0800

    update

--- src/modules/pay/preacher/ExperienceDay.tsx
+++ src/modules/pay/preacher/ExperienceDay.tsx
@@ -2,10 +2,7 @@ import * as React from 'react'
 import './ExperienceDay.less'
 import { connect } from 'react-redux'
 import { set, startLoad, endLoad, alertMsg } from 'redux/actions'
-import { SubmitButton } from '../../../components/submitbutton/SubmitButton'
-import { loadPreacherNumber } from './async'
 import { mark } from 'utils/request'
-import { MarkBlock } from '../components/markblock/MarkBlock'
 
 @connect(state => state)
 export default class ExperienceDay extends React.Component<any, any> {
@@ -17,16 +14,12 @@ export default class ExperienceDay extends React.Component<any, any> {
   constructor() {
     super()
     this.state = {}
+    this.pd = 50 / 750 * window.innerWidth
+    this.topPd = 90 / 500 * window.innerWidth
   }
 
   componentWillMount() {
-    mark({ module: '打点', function: '宣讲课页面', action: '进入体验日页面' })
-  }
-
-  handleClickGoApply() {
-    this.context.router.push({
-      pathname: '/pay/bsstart'
-    })
+    mark({ module: '打点', function: '体验日', action: '进入体验日页面' })
   }
 
   render() {
@@ -34,24 +27,28 @@ export default class ExperienceDay extends React.Component<any, any> {
 
     return (
       <div className="experience-day-page">
-        <div className="recently-tips">
-          <br/><br/>
-          <b
-            style={{ fontSize: '24px', display: 'block', textAlign: 'center', color: 'rgb(244,180,63)' }}>圈外商学院开放日申请</b>
-          <br/>
+        <div className="gutter" style={{ height: `${this.topPd}px` }}/>
+        <div className="success-header">预约成功</div>
+        <div className="success-tips">
+          <div>体验一：教学理念和服务</div>
+          <div>体验二：一节免费体验课</div>
+          <div>体验三：商学院面试指南</div>
+        </div>
+        <div className="step-wrapper">
+          <div className="content">
+            <div className="step step-1" data-step="1" style={{ paddingBottom: `${this.pd}px` }}>
+              扫码添加圈外小V
+              <div style={{fontSize:'11px',marginTop:'5px'}}>工作日2小时内回复，请耐心等待</div>
+              <img src="https://static.iqycamp.com/images/fragment/qrcode_xiaov_20180403.jpeg?imageslim" alt="小V"
+                   className="qrcode"/>
+            </div>
 
-          <div className="img-wrapper">
-            <img src="https://static.iqycamp.com/images/fragment/qrcode_xiaov_20180403.jpeg?imageslim"
-                 className="qrcode"/>
-          </div>
-          <div style={{textAlign:'center'}}>
-            添加“圈外小V”<br/>
-            回复“<b style={{ color: '#FFB200' }}>商学院</b>”开始体验吧
+            <div className="step step-3" data-step="2">
+              通过后<br/>
+              回复"商学院"开始体验！
+            </div>
           </div>
         </div>
-        {/*<MarkBlock module={'打点'} func={'体验日页面'} action={'申请商学院'}>*/}
-        {/*<SubmitButton clickFunc={() => this.handleClickGoApply()} buttonText="申请商学院"/>*/}
-        {/*</MarkBlock>*/}
       </div>
     )
   }
