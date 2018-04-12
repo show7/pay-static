3d0115f76cb93e7e720c24e2dcdfbdb6b2960a00
nethunder <17089113140@163.com>
2018-04-13 00:17:49 +0800

    update

--- src/modules/base/Base.tsx
+++ src/modules/base/Base.tsx
@@ -75,6 +75,7 @@ export default class Main extends React.Component<any, any> {
           server_url: `https://quanwai.cloud.sensorsdata.cn:4006/sa?token=0a145b5e1c9814f4&project=${window.ENV.sensorsProject}`,
           heatmap: {},
           is_single_page: true,
+          show_log: false
         });
         if(!!res.msg.riseId) {
           sa.login(res.msg.riseId);
