686774b71569e0d8d5c14a3d9e4666bb06cfcb23
nethunder <17089113140@163.com>
2018-04-16 18:04:13 +0800

    update

--- src/modules/pay/risepay/components/SaleBody.tsx
+++ src/modules/pay/risepay/components/SaleBody.tsx
@@ -197,7 +197,7 @@ export class SaleBody extends React.Component {
                onClick={() => this.clickUserProtocol()}>商学院用户协议</u>
           </div>
           <img className="pic-part3"
-               src="https://static.iqycamp.com/images/fragment/thought_sale_page_botton_2.png?imageslim"
+               src="https://static.iqycamp.com/images/fragment/thought_sale_page_botton_2.jpg?imageslim"
                style={{ width: '100%' }}/>
           {
             loading &&
