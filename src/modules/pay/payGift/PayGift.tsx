f53555ad16a4f9bf30728ed11ea076cef3cc345b
lance <451230805@qq.com>
2018-07-24 16:59:09 +0800

    ProjectGift

--- src/modules/pay/payGift/PayGift.tsx
+++ src/modules/pay/payGift/PayGift.tsx
@@ -156,10 +156,10 @@ export default class PayGift extends React.Component<any, any> {
     handlePayDone(){
         this.setState({showPayFlay:false,choseFlay:false,showShare:true});
         configShare(
-            '学习礼品卡',
+            `${window.ENV.userName}送你一张【圈外同学】训练营体验卡`,
             `https://${document.domain}/rise/activity/static/promotion/shareposter?memberTypeId=${this.state.memberTypeId}&riseId=${window.ENV.riseId}`,
             'https://static.iqycamp.com/3451532075605_-w6qw0nsj.pic.jpg',
-            `${window.ENV.userName}送你一张学习礼品卡\n点击领取，免费体验《${this.state.projectName}》`,
+            `点击领取，免费体验《${this.state.projectName}》`,
         )
     }
     /**
