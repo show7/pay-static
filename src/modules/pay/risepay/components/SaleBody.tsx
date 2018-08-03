f59bf206e003c2bb5d80baa0d55015c10720ddad
甄子康 <13122535350@163.com>
2018-08-03 23:53:23 +0800

    update

--- src/modules/pay/risepay/components/SaleBody.tsx
+++ src/modules/pay/risepay/components/SaleBody.tsx
@@ -189,13 +189,13 @@ export class SaleBody extends React.Component {
              id="business-school-intro-pic-container">
           <SequenceDisplay imgList={[
             mergeStyle({
-              url: 'https://static.iqycamp.com/images/pay_rise_part1_0731_1.jpg'
+              url: 'https://static.iqycamp.com/images/pay_l2_part1_0803_1.jpeg'
             }),
             // {
             //   dom: <CustomerEvaluate/>
             // },
             mergeStyle({
-              url: 'https://static.iqycamp.com/images/pay_rise_part2_0708_1.jpg'
+              url: 'https://static.iqycamp.com/images/pay_l2_part2_0803_1.jpeg'
             }), {
               dom: <div className="protocol-container l2">
                 <span className="click_text">点击查看</span>
@@ -254,13 +254,13 @@ export class SaleBody extends React.Component {
              id="business-school-intro-pic-container">
           <SequenceDisplay imgList={[
             mergeStyle({
-              url: 'https://static.iqycamp.com/images/pay_l1_part1_0711_1.jpg'
+              url: 'https://static.iqycamp.com/images/pay_l1_part1_0803_1.jpeg'
             }),
             // {
             //   dom: <CustomerEvaluate/>
             // },
             mergeStyle({
-              url: 'https://static.iqycamp.com/images/pay_l1_part2_0711_2.jpg'
+              url: 'https://static.iqycamp.com/images/pay_l1_part2_0803_1.jpeg'
             }), {
               dom: <div className="protocol-container l1">
                 <span className="click_text">点击查看</span>
