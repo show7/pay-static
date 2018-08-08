34cc0e1da3e1f7aeeb316af469e34338df2d7a4b
甄子康 <13122535350@163.com>
2018-08-08 14:46:52 +0800

    update

--- src/modules/pay/risepay/components/SaleBody.tsx
+++ src/modules/pay/risepay/components/SaleBody.tsx
@@ -254,7 +254,7 @@ export class SaleBody extends React.Component {
              id="business-school-intro-pic-container">
           <SequenceDisplay imgList={[
             mergeStyle({
-              url: 'https://static.iqycamp.com/images/pay_l1_part1_0805_1.jpg'
+              url: 'https://static.iqycamp.com/images/pay_l1_part1_0808_1.jpeg'
             }),
             // {
             //   dom: <CustomerEvaluate/>
