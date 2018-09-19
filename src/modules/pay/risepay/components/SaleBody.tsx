bc6e22024bfe7c355bcf53dc24082bc9a066cabe
甄子康 <13122535350@163.com>
2018-09-19 14:13:54 +0800

    update

--- src/modules/pay/risepay/components/SaleBody.tsx
+++ src/modules/pay/risepay/components/SaleBody.tsx
@@ -285,7 +285,7 @@ export class SaleBody extends React.Component {
              id="business-school-intro-pic-container">
           <SequenceDisplay imgList={[
             mergeStyle({
-              url: 'https://static.iqycamp.com/WechatIMG421-t0t8fzqc.jpeg'
+              url: 'https://static.iqycamp.com/L3-01@2x-s9010e2v.jpg'
             }),
             // {
             //   dom: <div className="hig1h-light-words" onClick={() => {
@@ -300,10 +300,10 @@ export class SaleBody extends React.Component {
               dom: <QYVideo fileId='5285890780601189027' videoPoster='https://static.iqycamp.com/images/thought_poster_0723.jpeg?imageslim'/>
             },
             mergeStyle({
-              url: 'https://static.iqycamp.com/WechatIMG422-5dzsgeym.jpeg'
+              url: 'https://static.iqycamp.com/L3-02@2x-dtgdssjj.jpg'
             }),
             mergeStyle({
-              url: 'https://static.iqycamp.com/WechatIMG423-perz804f.jpeg'
+              url: 'https://static.iqycamp.com/L3-03@2x-of8nu2w7.jpg'
             }), {
               dom: <div className="protocol-container thought">
                 <div>本课程全部内容版权归圈外同学所有，严禁翻录成任何形式或在第三方平台传播，违者将追究法律责任。</div>
