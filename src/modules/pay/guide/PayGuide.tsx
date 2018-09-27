5fec79b3bc4f54b1873e28c86a452e4ff5d9456a
三十文 <1047210087@qq.com>
2018-09-27 19:11:12 +0800

    remove coupon alert

--- src/modules/pay/guide/PayGuide.tsx
+++ src/modules/pay/guide/PayGuide.tsx
@@ -13,56 +13,38 @@ export default class PayGuide extends React.Component {
     router: React.PropTypes.object.isRequired,
   }
 
-  constructor() {
+  constructor () {
     super()
-    this.state = {
-      showCouponAlert: false,
-      totalAmount: 0,
-    }
+    this.state = {}
   }
 
-  async componentDidMount() {
+  async componentDidMount () {
     mark({ module: '打点', function: '售卖介绍页', action: '进入页面' })
     configShare(
       `【圈外同学】职场提升计划`,
       `https://${window.location.hostname}/pay/guide`,
       `https://static.iqycamp.com/71527579350_-ze3vlyrx.pic_hd.jpg`,
-      `${window.ENV.userName}邀请你成为同学，每天30分钟，开启你的职场进阶之旅`
+      `${window.ENV.userName}邀请你成为同学，每天30分钟，开启你的职场进阶之旅`,
     )
-    let couponsRes = await loadPersonalCoupons()
-    if(couponsRes && couponsRes.code === 200 && parseInt(couponsRes.msg.total) > 0) {
-      this.setState({
-        showCouponAlert: true,
-        totalAmount: couponsRes.msg.total,
-      })
-    }
   }
 
-  handleClickGoL1() {
+  handleClickGoL1 () {
     mark({ module: '打点', function: '售卖介绍页', action: '点击 L1' })
     window.location.href = '/pay/l1'
   }
 
-  handleClickGoL2() {
+  handleClickGoL2 () {
     mark({ module: '打点', function: '售卖介绍页', action: '点击 L2' })
     window.location.href = '/pay/rise'
   }
 
-  handleClickGoL3() {
+  handleClickGoL3 () {
     mark({ module: '打点', function: '售卖介绍页', action: '点击 L3' })
     window.location.href = '/pay/thought'
   }
 
-  handleHideAlert() {
-    mark({ module: '打点', function: '售卖介绍页', action: '点击隐藏弹框' })
-    this.setState({ showCouponAlert: false })
-  }
-
-  render() {
-    const {
-      showCouponAlert,
-      totalAmount,
-    } = this.state
+  render () {
+    const {} = this.state
 
     return (
       <div className="pay-guide-component">
@@ -74,11 +56,6 @@ export default class PayGuide extends React.Component {
              onClick={() => this.handleClickGoL2()}></div>
         <div className="click-block click-block-l3"
              onClick={() => this.handleClickGoL3()}></div>
-        {
-          showCouponAlert &&
-          <CouponAlert onClick={() => this.handleHideAlert()}
-                       amount={totalAmount}/>
-        }
       </div>
     )
   }
