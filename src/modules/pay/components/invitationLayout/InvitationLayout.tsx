6bf35fc4bfca3a6d7703a73f37aff247cde05822
dingzj <justin@iquanwai.com>
2018-11-13 11:00:21 +0800

    change name

--- src/modules/pay/components/invitationLayout/InvitationLayout.tsx
+++ src/modules/pay/components/invitationLayout/InvitationLayout.tsx
@@ -14,7 +14,7 @@ export default class InvitationLayout extends React.Component {
       <div className="invitation-layout">
         <div className="layout-box">
           <h3>好友邀请</h3>
-          <p>{oldNickName}觉得《{projectName}》很适合你，邀请你成为TA的同学，送你一张{amount}元的学习优惠券。</p>
+          <p>{oldNickName}觉得《{projectName}》很适合你，邀请你成为TA的同学，送你一张{amount}元的{projectName}专属学习优惠券。</p>
           <span className="button"
                 onClick={callBack}>知道了</span>
         </div>
