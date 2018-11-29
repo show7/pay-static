b17536325dc6b4ade4a24846632d86da7f0edfd0
甄子康 <13122535350@163.com>
2018-11-29 17:18:50 +0800

    update gp

--- src/modules/pay/risepay/AlipayReturn.tsx
+++ src/modules/pay/risepay/AlipayReturn.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { GoodsType } from '../../../utils/helpers'
 import { queryOrderSuccess } from './async'
-import { alertMsg } from '../../../redux/actions'
+import { alertMsg, endLoad } from '../../../redux/actions'
 import * as _ from 'lodash';
 
 export default class AlipayReturn extends React.Component<any, any> {
@@ -12,30 +12,36 @@ export default class AlipayReturn extends React.Component<any, any> {
   }
 
   componentWillMount() {
-    if(!!window.ENV.Detected.browser.name && window.ENV.Detected.browser.name === '微信') {
-      // 微信里
-      let orderId = _.get(location, 'query.orderId');
-      if(!!orderId) {
-        let orderInterval = setInterval(() => {
-          queryOrderSuccess(_.get(location, 'query.orderId')).then(res => {
-            if(res.code === 200) {
-              clearInterval(orderInterval);
-              const { goodsId, goodsType } = res.msg;
-              if(goodsType == GoodsType.FRAG_MEMBER || goodsType == GoodsType.FRAG_CAMP) {
-                window.location.href = `/pay/member/success?goodsId=${goodsId}`
-              } else if(goodsType == GoodsType.BS_APPLICATION) {
-                window.location.href = `/pay/applysubmit?goodsId=${goodsId}`
-              }
-            }
-          }).catch(ex => {
-            dispatch(alertMsg(ex));
-          })
-        }, 7000);
-      }
-    }
+
   }
 
   componentDidMount() {
+    let interval = setInterval(() => {
+      if(!!window.ENV.Detected.browser.name) {
+        clearInterval(interval);
+        if(window.ENV.Detected.browser.name === '微信') {
+          // 微信里
+          let orderId = _.get(location, 'query.orderId');
+          if(!!orderId) {
+            let orderInterval = setInterval(() => {
+              queryOrderSuccess(_.get(location, 'query.orderId')).then(res => {
+                if(res.code === 200) {
+                  clearInterval(orderInterval);
+                  const { goodsId, goodsType } = res.msg;
+                  if(goodsType == GoodsType.FRAG_MEMBER || goodsType == GoodsType.FRAG_CAMP) {
+                    window.location.href = `/pay/member/success?goodsId=${goodsId}`
+                  } else if(goodsType == GoodsType.BS_APPLICATION) {
+                    window.location.href = `/pay/applysubmit?goodsId=${goodsId}`
+                  }
+                }
+              }).catch(ex => {
+                console.error(ex);
+              })
+            }, 7000);
+          }
+        }
+      }
+    }, 100)
   }
 
   render() {
